43cf272304121ae79e98b6a5396f761d2c57b58b
Mary Dick <maryclairedick@gmail.com>
2020-08-24 16:38:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do |element|
     count = 0
-    while count < valid_calls.count do |v|
+    while count < valid_calls.length do |v|
       if element == v
         element
       end
